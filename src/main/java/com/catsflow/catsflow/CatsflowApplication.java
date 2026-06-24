cede32ff3ea6499eda6970cbaf4efc7ad6dfdc45
kanata0306 <kanata2395@gmail.com>
2026-06-24 23:25:21 +0900

    first commit

---
+++ src/main/java/com/catsflow/catsflow/CatsflowApplication.java
@@ -0,0 +1,13 @@
+package com.catsflow.catsflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catsflow/catsflow/CatsflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catsflow.catsflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
